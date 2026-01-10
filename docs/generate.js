27613eb19f6dba1219f0a517797b8ab377afc0e0
Mark Sample <samplereality@gmail.com>
2026-01-09 23:49:35 -0500

    switched rita from module to global

--- docs/generate.js
+++ docs/generate.js
@@ -1,21 +1,8 @@
 // Subcutanean Generator - JavaScript port of the Quant source code
 // Uses Rita.js for natural language processing
 
-// Import Rita.js as ES module
-import { RiTa } from "https://esm.sh/rita";
-
-// Set up some context to use in the grammar
-      let context = {
-        pluralNoun: () => RiTa.randomWord({ pos: "nns" }),
-        randomNoun: () => RiTa.randomWord({ pos: "nn" }),
-        randomVerb: () => RiTa.randomWord({ pos: "vb" }),
-        randomAdjective: () => RiTa.randomWord({ pos: "jj" }),
-        randomAdverb: () => RiTa.randomWord({ pos: "rb" }),
-        todaysDate: () => dayjs().format("MMMM D, YYYY"),
-        todaysTime: () => dayjs().format("h:mma"),
-      };
-// Import the grammar definition
-import createGrammar from "./grammar.js";
+// RiTa is loaded globally via CDN script tag
+// createGrammar is loaded from grammar.js
 
 // ============================================================================
 // GLOBAL STATE
@@ -24,76 +11,16 @@ import createGrammar from "./grammar.js";
 let currentSeed = 45443;
 let generatedText = '';
 
-// ============================================================================
-// RANDOM NUMBER GENERATOR (Seeded)
-// ============================================================================
-
-// Simple seeded random number generator (LCG algorithm)
-// This ensures reproducible results for the same seed
-class SeededRandom {
-    constructor(seed) {
-        this.seed = seed;
-    }
-
-    // Returns a random number between 0 and 1
-    random() {
-        this.seed = (this.seed * 1103515245 + 12345) % 2147483648;
-        return this.seed / 2147483648;
-    }
-
-    // Returns a random integer between min (inclusive) and max (inclusive)
-    randInt(min, max) {
-        return Math.floor(this.random() * (max - min + 1)) + min;
-    }
-
-    // Returns a random element from an array
-    choice(array) {
-        return array[Math.floor(this.random() * array.length)];
-    }
-}
-
-// ============================================================================
-// QUANT PARSER AND COLLAPSER
-// ============================================================================
-
-// This will contain the main logic for parsing and collapsing Quant markup
-// Port of the Python collapser.py functionality
-
-class QuantCollapser {
-    constructor(seed) {
-        this.rng = new SeededRandom(seed);
-        this.variables = {};
-        this.macros = {};
-    }
-
-    // Parse and collapse Quant markup into plain text
-    collapse(text) {
-        // TODO: Implement Quant markup parsing
-        // This is where we'll port the Python collapser logic
-
-        // For now, return a placeholder
-        return `[Generated text with seed ${this.rng.seed}]\n\nThis is a placeholder. The Quant parser will be implemented here.`;
-    }
-
-    // Handle variable assignments [@varname|option1|option2]
-    handleVariable(match) {
-        // TODO: Implement variable handling
-        return '';
-    }
-
-    // Handle conditionals [condition>text]
-    handleConditional(match) {
-        // TODO: Implement conditional handling
-        return '';
-    }
-
-    // Handle choices [option1|option2|option3]
-    handleChoice(match) {
-        // TODO: Implement choice handling
-        const options = match.split('|');
-        return this.rng.choice(options);
-    }
-}
+// Context object for passing variables to RiTa grammar
+// You can add custom functions and values here that will be available
+// in the grammar via $symbolName notation
+let context = {
+    pluralNoun: () => RiTa.randomWord({ pos: "nns" }),
+    randomNoun: () => RiTa.randomWord({ pos: "nn" }),
+    randomVerb: () => RiTa.randomWord({ pos: "vb" }),
+    randomAdjective: () => RiTa.randomWord({ pos: "jj" }),
+    randomAdverb: () => RiTa.randomWord({ pos: "rb" })
+};
 
 // ============================================================================
 // UI EVENT HANDLERS
@@ -143,8 +70,8 @@ async function generateNovel() {
         // Set Rita's random seed for deterministic generation
         RiTa.randomSeed(currentSeed);
 
-        // Load the grammar
-        const grammar = createGrammar(RiTa);
+        // Load the grammar with context
+        const grammar = createGrammar(RiTa, context);
 
         // Generate text from the grammar
         const output = grammar.expand();
--- docs/grammar.js
+++ docs/grammar.js
@@ -1,4 +1,4 @@
-function createGrammar(RiTa) {
+function createGrammar(RiTa, context) {
     return RiTa.grammar({
         start: "$part1",
 
@@ -6,7 +6,7 @@ function createGrammar(RiTa) {
 
         epigraph1: "$MacEwen (40) | $Mouawad (30) | $Teasdale (20) | $HerbertCalvino (7) | $Brunt (3)",
 
-        MacEwen: "<em>This land like a mirror turns you inward<br>And you become a forest in a furtive lake;<br>The dark pines of your mind reach downward,<br>You dream in the green of your time,<br>Your memory is a row of sinking pines.</em><br><br>“Dark Pines Under Water,” Gwendolyn MacEwen (1941-1987)",
+        MacEwen: "<em>This land like a mirror turns you inward<br>And you become a forest in a furtive lake;<br>The dark pines of your mind reach downward,<br>You dream in the green of your time,<br>Your memory is a row of sinking pines.</em><br><br>\"Dark Pines Under Water,\" Gwendolyn MacEwen (1941-1987)",
 
         Mouawad: "<em>Icarus should have waited for nightfall,<br>the moon would have never let him go.</em><br><br>Nina Mouawad",
 
@@ -14,12 +14,10 @@ function createGrammar(RiTa) {
 
         HerbertCalvino: "<em>Deep in the human unconscious is a pervasive need for a logical universe that makes sense. But the real universe is always one step beyond logic.</em><br><br>Frank Herbert<br><br><em>One cannot write about something one is still inside.</em><br><br>Italo Calvino",
 
-        Brunt: "<em>Maybe I was destined to forever fall in love with people I couldn’t have. Maybe there’s a whole assortment of impossible people waiting for me to find them. Waiting to make me feel the same impossibility over and over again.</em><br><br>From “Tell the Wolves I'm Home,” by Carol Rifka Brunt",
+        Brunt: "<em>Maybe I was destined to forever fall in love with people I couldn't have. Maybe there's a whole assortment of impossible people waiting for me to find them. Waiting to make me feel the same impossibility over and over again.</em><br><br>From \"Tell the Wolves I'm Home,\" by Carol Rifka Brunt",
 
         opening: "<p>[I can't tell you this$maybech1Niko. I don't want|I don't know how to tell you this$maybech1Niko. I never wanted|I'm not sure how to tell you this$maybech1Niko. I don't want|I don't want to tell you this$maybech1Niko. I don't want] to gut you, reach inside and pull things out, not again. [Old wounds and sleeping dogs, you know. Tales|Some tales, I guess, are] better left untold. And you've [heard this one before, even|already heard this one, even|heard this one, haven't you? Even] if [your story wasn't quite the same as mine|our stories were never quite the same|your version never quite matched mine|our stories, our endings, never quite agreed].</p><p> [But that's what sat me down to write.|But that's why I'm sitting here, writing this.|But that's what this is for. Why I wrote it.|But that's what writing this is for.] If it's [just | only | ] a story, [ | you see,| nothing else,] maybe we can understand[&nbsp;it. You know | ], [come to terms. Make peace.|come to terms.|make peace.] [<br><br> | ]Pretend it's not ours.</p>",
 
         maybech1Niko: " | | | | | | | |, Niko|, Niko"
-    });
+    }, context);
 }
-
-export default createGrammar;
